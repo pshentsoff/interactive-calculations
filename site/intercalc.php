8d4d62befd1eae1f4f328c2bdcca871d8316de43
V.Pshentsov <pshentsoff@gmail.com>
2013-10-31 18:26:56 +0600

    First commit. Structure

---
+++ site/intercalc.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file        intercalc.php
+ * @description
+ *
+ * PHP Version  5.3.13
+ *
+ * @package 
+ * @category
+ * @plugin URI
+ * @copyright   2013, Vadim Pshentsov. All Rights Reserved.
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @author      Vadim Pshentsov <pshentsoff@gmail.com> 
+ * @link        http://pshentsoff.ru Author's homepage
+ * @link        http://blog.pshentsoff.ru Author's blog
+ *
+ * @created     31.10.13
+ */
+
+?>
+
+Interactive Calculations!
---
+++ site/router.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file        router.php
+ * @description
+ *
+ * PHP Version  5.3.13
+ *
+ * @package 
+ * @category
+ * @plugin URI
+ * @copyright   2013, Vadim Pshentsov. All Rights Reserved.
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @author      Vadim Pshentsov <pshentsoff@gmail.com> 
+ * @link        http://pshentsoff.ru Author's homepage
+ * @link        http://blog.pshentsoff.ru Author's blog
+ *
+ * @created     31.10.13
+ */
